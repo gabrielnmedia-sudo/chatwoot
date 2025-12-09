d67a5ed138259269029a7e9792b8cc9f89dd16c2
gabrielnmedia-sudo <gabrielnmedia@gmail.com>
2025-12-09 03:09:26 -0800

    fix(tokens): Raise error if TwiML App SID is unset

--- app/controllers/api/v1/accounts/twilio/tokens_controller.rb
+++ app/controllers/api/v1/accounts/twilio/tokens_controller.rb
@@ -4,6 +4,12 @@ class Api::V1::Accounts::Twilio::TokensController < Api::V1::Accounts::BaseContr
     identity = current_user.id.to_s
 
     # Create Voice grant for your Twilio capability
+    # Guard Clause: Ensure the SID is present
+    if ENV['TWILIO_TWIML_APP_SID'].blank?
+      Rails.logger.error "TWILIO_ERROR: ENV['TWILIO_TWIML_APP_SID'] is missing!"
+      raise ArgumentError, "Missing TWILIO_TWIML_APP_SID in Environment"
+    end
+
     grant = Twilio::JWT::AccessToken::VoiceGrant.new
     grant.outgoing_application_sid = ENV['TWILIO_TWIML_APP_SID']
     grant.incoming_allow = true # Allow incoming calls to this client
